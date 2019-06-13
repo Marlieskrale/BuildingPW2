2feac0fba0bffa54a60013c49fb71f9d54a36e8e
marlieskrale <marlies.krale@student.hku.nl>
2019-06-13 11:50:03 +0200

    Camera stopt volgen

---
+++ BuildingPW2/Assets/CameraReleaseTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraReleaseTrigger : MonoBehaviour
+{
+    public GameObject player;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "CameraReleaseTrigger")
+        {
+            Debug.Log("Camera stopt met meelopen");
+            GameObject.Find("Main Camera").transform.parent = null;
+        }
+
+    }
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("camera is a free bird");
+        GameObject.Find("Main Camera").transform.parent = player.transform;
+    }
+}
