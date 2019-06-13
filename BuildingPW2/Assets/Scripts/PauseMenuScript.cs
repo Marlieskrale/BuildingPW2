20662775dbf6f301c210f5b64170fde4469fa34d
marlieskrale <marlies.krale@student.hku.nl>
2019-06-13 22:39:08 +0200

    Met geluid

---
+++ BuildingPW2/Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume ()
+    {
+        Debug.Log("Resume");
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause ()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Quit ()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
